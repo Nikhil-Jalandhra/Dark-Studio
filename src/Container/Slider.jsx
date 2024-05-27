3a62b98c16302c08f5c2bfe9e91064d3a7eed249
Nikhil-Jalandhra <jneek36@gmail.com>
2024-05-27 23:34:55 +0530

    working on slider component

--- src/Container/Slider.jsx
+++ src/Container/Slider.jsx
@@ -6,16 +6,38 @@ import Profile from '../Images/Profile.jpg'
 
 function Slider() {
 
+  const data = [
+    "firstData" [
+      {
+        image : "../Images/Profile.jpg",
+        heading: "From Shadows to Spotlight.<br />Dark-Studio Doubles Sales in Record Time!",
+        title: "Nikhil Jalandhra <BsDot/> JNEEK <BsDot/> Founder, Dark Studio"
+      }
+    ],
+    "secondData" [
+      {
+        image : "../Images/Profile.jpg",
+        heading: "Hello",
+        title: "Hello"
+      }
+    ]
+  ]
+
+  const changeImage = () => {
+
+  }
+
 
   return (
     <div className='w-full h-screen relative'>
 
-        <div className='w-full flex justify-between items-center px-[40px] h-screen'>
-            <div className='w-[80px] h-[480px] flex justify-center items-center border-[1px] border-gray-800'><TfiArrowLeft/></div>
-            <div className='w-[80px] h-[480px] flex justify-center items-center border-[1px] border-gray-800'><TfiArrowRight/></div>
+        <div className='w-full absolute z-[10] flex justify-between items-center px-[40px] h-screen'>
+            <div className='w-[80px] h-[480px] flex justify-center items-center border-[1px] border-gray-800' onClick={changeImage}><TfiArrowLeft/></div>
+            <div className='w-[80px] h-[480px] flex justify-center items-center border-[1px] border-gray-800' onClick={changeImage}><TfiArrowRight/></div>
         </div>
 
-        <div className='w-full h-screen absolute flex justify-center text-center top-0'>
+        {data.map((source,index)=> (
+          <div key={index} className='w-full h-screen absolute flex justify-center text-center top-0'>
           <div className='w-[200px] h-[195px] absolute top-[80px] opacity-[0.9] rounded-lg overflow-hidden'>
               <img src={Profile} alt="" />
           </div>
@@ -27,6 +49,7 @@ function Slider() {
               
               <p className='absolute uppercase flex items-center text-[18px] font-normal top-[500px]'>Nikhil Jalandhra <BsDot/> JNEEK <BsDot/> Founder, Dark Studio</p>
         </div>
+        ))}
 
     </div>
   );
