cc9c90a490e783d98d7945f4a36ec84520faeb29
Nikhil-Jalandhra <jneek36@gmail.com>
2024-05-28 11:05:30 +0530

    content is completed now time to animate

--- src/Container/Slider.jsx
+++ src/Container/Slider.jsx
@@ -1,30 +1,35 @@
-import React from 'react';
+import {React, useState} from 'react';
 import { TfiArrowRight } from "react-icons/tfi";
 import { TfiArrowLeft } from "react-icons/tfi";
 import { BsDot } from "react-icons/bs";
 import Profile from '../Images/Profile.jpg'
+import Profile1 from '../Images/Profile1.jpg'
 
 function Slider() {
 
-  const data = [
-    "firstData" [
+  const content = {
+    data: [
       {
-        image : "../Images/Profile.jpg",
-        heading: "From Shadows to Spotlight.<br />Dark-Studio Doubles Sales in Record Time!",
-        title: "Nikhil Jalandhra <BsDot/> JNEEK <BsDot/> Founder, Dark Studio"
-      }
-    ],
-    "secondData" [
+        image : Profile,
+        heading1: "From Shadows to Spotlight.",
+        heading2: "Dark-Studio Doubles Sales in Record Time!",
+        name: "JNEEK",
+        founder: "Founder, Dark Studio"
+      },
       {
-        image : "../Images/Profile.jpg",
-        heading: "Hello",
-        title: "Hello"
+        image : Profile1,
+        heading1: "Code by code, pixel by pixel,",
+        heading2: "we're weaving the fabric of digital innovation",
+        name: "Nikhil Jalandhra",
+        founder: "Founder, Skull Event"
       }
     ]
-  ]
+  }
 
-  const changeImage = () => {
+  const [changePath, setChangePath] = useState(0);
 
+  const changeContent = () => {
+    changePath === 0 ? setChangePath(1) : setChangePath(0)
   }
 
 
@@ -32,24 +37,25 @@ function Slider() {
     <div className='w-full h-screen relative'>
 
         <div className='w-full absolute z-[10] flex justify-between items-center px-[40px] h-screen'>
-            <div className='w-[80px] h-[480px] flex justify-center items-center border-[1px] border-gray-800' onClick={changeImage}><TfiArrowLeft/></div>
-            <div className='w-[80px] h-[480px] flex justify-center items-center border-[1px] border-gray-800' onClick={changeImage}><TfiArrowRight/></div>
+            <div className='w-[80px] h-[480px] flex justify-center items-center border-[1px] border-gray-800' onClick={changeContent}><TfiArrowLeft/></div>
+            <div className='w-[80px] h-[480px] flex justify-center items-center border-[1px] border-gray-800' onClick={changeContent}><TfiArrowRight/></div>
         </div>
 
-        {data.map((source,index)=> (
-          <div key={index} className='w-full h-screen absolute flex justify-center text-center top-0'>
+          <div className='w-full h-screen absolute flex justify-center text-center top-0'>
           <div className='w-[200px] h-[195px] absolute top-[80px] opacity-[0.9] rounded-lg overflow-hidden'>
-              <img src={Profile} alt="" />
+              <img src={content.data[changePath].image} alt="" />
           </div>
   
-              <p className=' text-[120px] leading-[120px] absolute top-[200px]'
+              <p className=' text-[120px] leading-[120px] absolute top-[200px] uppercase'
               style={{fontFamily : "mainFont"}}>
-                      "From Shadows to Spotlight.<br />Dark-Studio Doubles Sales in Record Time!"
+                     " {content.data[changePath].heading1}<br />{content.data[changePath].heading2}"
               </p>
               
-              <p className='absolute uppercase flex items-center text-[18px] font-normal top-[500px]'>Nikhil Jalandhra <BsDot/> JNEEK <BsDot/> Founder, Dark Studio</p>
+              <p className='absolute uppercase flex 
+              items-center text-[18px] font-normal top-[500px]'>
+                {content.data[changePath].name} <BsDot/> {content.data[changePath].founder}
+              </p>
         </div>
-        ))}
 
     </div>
   );
