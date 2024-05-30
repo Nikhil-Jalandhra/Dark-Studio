721f30d6e037c1420e64638b7b72ea572954efab
Nikhil-Jalandhra <jneek36@gmail.com>
2024-05-30 21:04:48 +0530

    Working on footer

--- src/Container/Footer.jsx
+++ src/Container/Footer.jsx
@@ -4,16 +4,12 @@ import React from 'react';
 function Footer() {
   return (
     <div className='w-full h-screen py-[20px] px-[40px]'>
-      <div className='w-full bg-slate-200 rounded-lg overflow-hidden'>
-        <div className=' flex item-center whitespace-nowrap text-[150px] uppercase' style={{fontFamily: "mainFont"}}>
-            <p>Let Work Together</p>
-            <p className='mx-[30px]'>-</p>
-            <p>Let Work Together</p>
-            <p className='mx-[30px]'>-</p>
-            <p>Let Work Together</p>
-            <p className='mx-[30px]'>-</p>
-            <p>Let Work Together</p>
-            <p className='mx-[30px]'>-</p>
+      <div className='w-full h-[30vh] bg-slate-200 rounded-lg '>
+        <div className=' w-full h-full py-[20px] whitespace-nowrap flex items-center text-[150px] uppercase' style={{fontFamily: "mainFont"}}>
+            <div className=' h-full w-full leading-none bg-red-400 overflow-hidden containerFooter'>
+                <p className='animeFooter'>let work together</p>
+                <p className='animeFooter'>let work together</p>
+            </div>
         </div>
       </div>
       <div></div>
