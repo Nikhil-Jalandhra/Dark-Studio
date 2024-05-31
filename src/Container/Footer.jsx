0186608a8f6756d1847b5b939dd1bd8ca9dea275
Nikhil-Jalandhra <jneek36@gmail.com>
2024-05-31 20:59:36 +0530

    Working on footer

--- src/Container/Footer.jsx
+++ src/Container/Footer.jsx
@@ -1,18 +1,10 @@
-import { motion } from 'framer-motion';
 import React from 'react';
 
 function Footer() {
   return (
     <div className='w-full h-screen py-[20px] px-[40px]'>
-      <div className='w-full h-[30vh] bg-slate-200 rounded-lg '>
-        <div className=' w-full h-full py-[20px] whitespace-nowrap flex items-center text-[150px] uppercase' style={{fontFamily: "mainFont"}}>
-            <div className=' h-full w-full leading-none bg-red-400 overflow-hidden containerFooter'>
-                <p className='animeFooter'>let work together</p>
-                <p className='animeFooter'>let work together</p>
-            </div>
-        </div>
+      <div className='h-[30vh] w-full bg-red-400'>
       </div>
-      <div></div>
     </div>
   );
 }
