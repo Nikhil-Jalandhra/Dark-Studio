4ca766f1ff97b9d75f53aa9b771f6e59e8392fd7
Nikhil-Jalandhra <jneek36@gmail.com>
2024-06-01 13:15:37 +0530

    Working on footer

--- src/Container/Footer.jsx
+++ src/Container/Footer.jsx
@@ -1,9 +1,40 @@
 import React from 'react';
+import { motion } from 'framer-motion';
 
 function Footer() {
   return (
     <div className='w-full h-screen py-[20px] px-[40px]'>
-      <div className='h-[30vh] w-full bg-red-400'>
+      <div className='h-[30vh] flex items-center py-[20px] w-full overflow-hidden containerFooter border-[1px] border-[#1E1F20] rounded-lg'>
+        <motion.div 
+        animate={{x: "-100%"}}
+        transition={{duration: 15, repeat: Infinity, ease: 'linear'}}
+        className='h-full flex items-center text-[160px] uppercase leading-none overflow-hidden' style={{minWidth: "fit-content", fontFamily: "mainFont"}}>
+          <div className='w-full whitespace-nowrap h-full animeFooter'>
+            <p className='animeFooter' >Let's work together</p>
+            <p className='animeFooter' >Let's work together</p>
+          </div>
+          <div className='px-[30px]'>-</div>
+          <div className='w-full whitespace-nowrap h-full animeFooter'>
+            <p className='animeFooter' >Let's work together</p>
+            <p className='animeFooter' >Let's work together</p>
+          </div>
+          <div className='px-[30px]'>-</div>
+        </motion.div>
+        <motion.div 
+        animate={{x: "-100%"}}
+        transition={{duration: 15, repeat: Infinity, ease: 'linear'}}
+        className='w-full h-full flex items-center text-[160px] uppercase leading-none overflow-hidden' style={{minWidth: "fit-content", fontFamily: "mainFont"}}>
+          <div className='w-full whitespace-nowrap h-full animeFooter'>
+            <p className='animeFooter' >Let's work together</p>
+            <p className='animeFooter' >Let's work together</p>
+          </div>
+          <div className='px-[30px]'>-</div>
+          <div className='w-full whitespace-nowrap h-full animeFooter'>
+            <p className='animeFooter' >Let's work together</p>
+            <p className='animeFooter' >Let's work together</p>
+          </div>
+          <div className='px-[10px]'>-</div>
+        </motion.div>
       </div>
     </div>
   );
