64450b90a6464018b0d23a22d850718b9f2e0b9a
Nikhil-Jalandhra <jneek36@gmail.com>
2024-09-20 22:24:56 +0530

    minor updates

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,8 @@ import Status from "./Container/Status"
 import Footer from "./Container/Footer"
 import ImagesPage from "./Container/ImagesPage"
 import LandingPage from "./Container/LandingPage"
-import locomotiveScroll from 'locomotive-scroll'
 
 function App() {
-
-  const smoothScroll = new locomotiveScroll();
-
-  smoothScroll
 
   return (
     <>
